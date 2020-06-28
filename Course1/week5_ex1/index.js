d017f35133e7a81a76964032b6a74994762915c1
Shytikova Yelyzaveta <izuma3kitsune@gmail.com>
2020-06-28 11:24:38 +0300

    done week 5 and course 1

--- Course1/week5_ex1/index.js
+++ Course1/week5_ex1/index.js
@@ -6,7 +6,8 @@ module.exports = {
      * @param {Function} handler
      */
     on: function (event, subscriber, handler) {
-
+        ruleEvents.add(event, subscriber, handler);
+        return this;
     },
 
     /**
@@ -14,13 +15,53 @@ module.exports = {
      * @param {Object} subscriber
      */
     off: function (event, subscriber) {
-
+        ruleEvents.remove(event, subscriber);
+        return this;
     },
 
     /**
      * @param {String} event
      */
     emit: function (event) {
-
+        if(ruleEvents.events.hasOwnProperty(event)){
+            ruleEvents.events[event].handlers.forEach(function(item, index){
+                item.call(ruleEvents.events[event].subscribers[index]);
+            })
+        }
+        return this;
     }
 };
+var ruleEvents = {
+    events: {},
+
+    add: function(event, subscriber, handler){
+        this.events[event]===undefined?this.events[event]={}:false;
+        if(!this.events[event].hasOwnProperty("subscribers"))
+        {
+            this.events[event]["subscribers"] = [];
+        }
+        this.events[event]["subscribers"].push(subscriber);
+        if(!this.events[event].hasOwnProperty("handlers"))
+        {
+            this.events[event]["handlers"] = [];
+        }
+        this.events[event]["handlers"].push(handler);
+    },
+    remove: function(event, subscriber){
+        var removeHandlers = [];
+        if(this.events[event]===undefined){
+            return;
+        }
+        this.events[event].subscribers =  this.events[event].subscribers.filter(function(item, index){
+            item===subscriber?removeHandlers.push(this.events[event].handlers[index]):false;
+            return item!==subscriber;
+        }.bind(this));
+        this.events[event].handlers = this.events[event].handlers.filter(function(item, index){
+            return removeHandlers.indexOf(item)===-1;
+        });
+        
+        if(this.events[event].subscribers.length===0){
+            delete this.events[event];
+        }
+    },
+}
